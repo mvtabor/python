1285cf2ed7299d466ccf8e3815d85a61d3648b97
Luis Santiago Romero Angulo <santiago-rom@hotmail.com>
2024-01-17 14:55:09 -0500

    DatosdeFormulario.py
    Al construir aplicaciones, será necesario enviar datos de formulario, una página web (HTML, CSS) a nuestro servidor, y desde Flask vamos a poder tomar estos datos y poder ejecutar acciones. 
    Para esto tendremos que construir un formulario completo o podemos utilizar la herramienta de curl.

---
+++ FLASK micro-framework/DatosdeFormulario.py
@@ -0,0 +1,21 @@
+from flask import Flask, request #1
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hello, world'
+
+@app.route('/libros', methods=['POST'])
+def libros():
+    print(request.form) #1
+    print(request.form['llave1']) #2
+    print(request.form['llave2'])
+    return 'libros'
+
+#1 haremos uso del objeto request para ver los datos enviados a través de un formulario.
+#2 lo utilizamos para obtener los datos en un formulario
+#EN OTRA VENTANA GITBASH
+
+# curl -d "llave1=dato1&llave2=dato2" -X POST http://localhost:5000/libros
+
+# curl nos permite realizar llamadas a nuestro servicio. Le indicamos el tipo de método a utilizar.
