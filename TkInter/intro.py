220c622c469cb1bfe15934ee5ec481fec8aeefc0
Luis S. Angulo <santiago-rom@hotmail.com>
2024-03-26 11:53:29 -0400

    intro.py

--- TkInter/intro.py
+++ TkInter/intro.py
@@ -4,9 +4,15 @@ root =Tk()
 root.title('Hola Mundo')
 root.geometry('400x500')
 
-label = Label(root, text='¡Hola mundo! Mi primera etiqueta.')
+#label = Label(root, text='¡Hola mundo! Mi primera etiqueta.')
 #Label(root, text='¡Hola mundo! Mi primera etiqueta.').pack()
+#label.pack() #no es necesario crear una nueva instancia de label y asignarla a una variable para luego agregarla con pack
+#  Esta forma es un poco límitada porque no le podemos entregar un orden. Por eso, podemos utilizar .grid.
 
-label.pack() #no es necesario crear una nueva instancia de label y asignarla a una variable para luego agregarla con pack
+l1= Label(root, text='¡Hola, mundo! Mi primera etiqueta.')
+l2= Label(root, text='¡Adiós, mundo! Segunda etiqueta.')
+
+l1.grid(row=0, column=0)
+l2.grid(row=1, column=1)
 
 root.mainloop()
