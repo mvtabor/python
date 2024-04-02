2c97eb68b1d07ded9a1b220995fa2f8abde29c19
Luis S. Angulo <santiago-rom@hotmail.com>
2024-04-02 12:59:20 -0400

    input.py

--- TkInter/input.py
+++ TkInter/input.py
@@ -10,15 +10,21 @@ e.insert(0, 'Ingrese texto:')
 
 def click():
     texto = e.get()
-    l = Label(root, text=texto)
-    l.pack()
+    textvariable.set(texto)
+    valor = textvariable.get()
+    print(valor)
+    #l = Label(root, text=texto)
+    #l.pack()
     e.delete(0, END)
     #l.configure(text=texto)
 
 btn = Button(root, text='click', command=click)
 btn.pack()
 
+textvariable = StringVar()
+
+l = Label(root, textvariable=textvariable)
 #l = Label(root, text='Texto de la etiqueta')
-#l.pack()
+l.pack()
 
 root,mainloop()
