826ea67b90f2a5374eb60905b41b3303758f6736
Luis S. Angulo <santiago-rom@hotmail.com>
2024-04-17 17:35:58 -0400

    radio.py
    No siempre vamos a tener definidas la opciones, muchas veces recibiremos una lista con los valores.

--- TkInter/radio.py
+++ TkInter/radio.py
@@ -6,10 +6,24 @@ root.title('Hello, World!')
 r = IntVar()
 r.set('2')
 
-Radiobutton(root, text='Opción 1', variable=r, value=1).pack()
-Radiobutton(root, text='Opción 2', variable=r, value=2).pack()
+#Radiobutton(root, text='Opción 1', variable=r, value=1).pack()
+#Radiobutton(root, text='Opción 2', variable=r, value=2).pack()
 
-l = Label(root, textvariable=r)
+RUNNERS = [
+    ('Veloz', 'Veloz'),
+    ('Lento', 'Lento'),
+    ('Animado', 'Animado'),    
+    ('Cansado', 'Cansado')    
+]
+
+runner = StringVar()
+runner.set('Cansado')
+
+for text, run in RUNNERS:
+    Radiobutton(root, text=text, variable=runner, value=run).pack()
+
+#l = Label(root, textvariable=r)
+l = Label(root, textvariable=runner)
 l.pack()
 
 root.mainloop()
