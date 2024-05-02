23e0c6c38e6cf6f344e9c27b44e654f1ec53ba8d
Luis S. Angulo <santiago-rom@hotmail.com>
2024-05-02 15:11:56 -0400

    treeview.py

--- TkInter/Gestor de clientes/treeview.py
+++ TkInter/Gestor de clientes/treeview.py
@@ -1,1 +1,29 @@
+from tkinter import *
+from tkinter import ttk
 
+root = Tk()
+root.title('Hello, world: treeview')
+
+tree = ttk.Treeview(root)
+tree['columns'] = ('Nombre', 'Telefono', 'Empresa')
+
+#tree.column('#0')
+tree.column('#0', width=0, stretch=NO)
+tree.column('Nombre')
+tree.column('Telefono')
+tree.column('Empresa')
+
+#tree.heading('#0', text='id')
+tree.heading('#0')
+tree.heading('Nombre', text='Nombre')
+tree.heading('Telefono', text='Telefono')
+tree.heading('Empresa', text='Empresa')
+
+tree.grid(column=0, row=0)
+
+tree.insert('', END, 'lala', values=('Uno', 'Dos', 'Tres'), text='Chanchito feliz')
+tree.insert('', END, 'lele', values=('Cuatro', 'Cinco', 'Seis'), text='Chanchito triste')
+tree.insert('', END, 'lili', values=('4', '5', '6'), text='Lechoncito')
+
+
+root.mainloop()
