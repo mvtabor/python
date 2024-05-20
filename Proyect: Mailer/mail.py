c26e3782c076118b28d55425294c958c97050059
Luis S. Angulo <santiago-rom@hotmail.com>
2024-05-20 09:47:32 -0400

    mail

---
+++ Proyect: Mailer/mail.py
@@ -0,0 +1,77 @@
+from flask import (
+    Blueprint, render_template, request, flash, redirect, url_for, current_app
+)
+
+import sendgrid
+from sendgrid.helpers.mail import *
+
+from app.db import get_db
+
+bp = Blueprint('mail', __name__, url_prefix="/")
+
+@bp.route('/', methods=['GET'])
+def index():
+    search = request.args.get('search')
+    db, c = get_db()
+    if search is None:
+        c.execute("SELECT * FROM email")
+    else:
+        c.execute("SELECT * from email WHERE content LIKE %s", ('%' + search + '%', ))  
+    mails = c.fetchall()
+
+    return render_template('mails/index.html', mails=mails)
+
+@bp.route('/create', methods=['GET', 'POST'])
+def create():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        subject = request.form.get('subject')
+        content = request.form.get('content')
+        errors = []
+
+
+        if not email:
+            errors.append('Email es obligatorio')
+        if not subject:
+            errors.append('Asunto es obligatio')
+        if not content:
+            errors.append('Contenido es obligatorio')
+
+
+        if len(errors) == 0:
+            send(email, subject, content)
+            db, c = get_db()
+            c.execute("INSERT INTO email (email, subject, content) VALUES (%s, %s, %s)", (email, subject, content))
+            db.commit()
+
+            return redirect(url_for('mail.index'))
+        else:
+            for error in errors:
+                flash(error)
+
+    return render_template('mails/create.html')
+
+def send(to, subject, content):
+    sg = sendgrid.SendGridAPIClient(api_key=current_app.config['SENDGRID_KEY'])
+    #from_email = Email("no-reply@myapp.com")
+    from_email = Email(current_app.config['FROM_EMAIL'])
+    to_email = To(to)
+    content = content('text/plain', content)
+    mail = Mail(from_email, to_email, subject, content)
+    response = sg.client.mail.send.post(request_body=mail.get())
+
+    print(response)
+
+
+
+
+
+
+
+
+
+
+#Crear una cuenta en Sendgrid: https://sendgrid.com/en-us/1?adobe_mc_sdid=SDID%3D14F2C4ADB6A64C84-2BA29866DE9211E1%7CMCORGID%3D32523BB96217F7B60A495CB6%40AdobeOrg%7CTS%3D1716202853&adobe_mc_ref=https%3A%2F%2Fsendgrid.com%2Fen-us%2F1
+#Dashboard - Settings - API Key (create) -full access
+#Crear cuenta en Heroku para desplegar en la nube
+#Heroku install cli -> follow installation method
