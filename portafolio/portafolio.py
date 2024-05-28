91033ba4fec6cee8984d136527495894dc0c2580
Luis S. Angulo <santiago-rom@hotmail.com>
2024-05-28 12:34:57 -0400

    portafolio.py

--- portafolio/portafolio.py
+++ portafolio/portafolio.py
@@ -7,3 +7,7 @@ bp = Blueprint('portafolio', __name__, url_prefix='/')
 @bp.route('/', methods=['GET'])
 def index():
     return render_template('portafolio/index.html')
+
+@bp.route('/mail', methods=['POST'])
+def mail():
+    return 'Chancho'
